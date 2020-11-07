6609932bbac3442ed565ea5c083c1cdd70513e77
Jon Silverman <jonsilverman@yahoo.com>
2020-11-07 15:44:38 -0600

    Initial commit of skeleton

---
+++ stock_port/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Stock Portfolio."""
+
+__author__ = """Jon S"""
+__email__ = 'jons@gmail.com'
+__version__ = '0.1.0'
---
+++ tests/test_stock_port.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `stock_port` package."""
+
+
+import unittest
+
+from stock_port import stock_port
+
+
+class TestStock_port(unittest.TestCase):
+    """Tests for `stock_port` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
